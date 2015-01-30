28c4216823d19d8461af4e754106381094666688
mandeepbhutani <bhutanimandeep@gmail.com>
2015-01-30 17:57:40 -0600

    Check if Palindrome

--- Palindrome.py
+++ Palindrome.py
@@ -1,20 +1,22 @@
 """
-Project: Sample Project from Projects Repo
+Project: Projects Repo - Check if Palindrome
+
 Author: Mandeep Bhutani
+
 Date: 1/30/2015
 
-Problem: Enter a number and have the program generate PI up to that many decimal places.
-Keep a limit to how far the program will go.
+Problem: Checks if the string entered by the user is a palindrome.
+That is that it reads the same forwards as backwards like “racecar”.
 
-References: 
+References: https://docs.python.org/2.3/whatsnew/section-slices.html
 """
 def palindrome():
-	givenword = str(raw_input("Please enter the word you would to have checked:"))
-	while not givenword.isalpha(): # Checks to make sure all characters are alpha characters
+	givenword = str(raw_input("Please enter the word you would like to have checked:"))
+	while not givenword.isalpha(): # Checks to make sure all characters are alpha characters.
 		print "Please enter a real word."
-		givenword = str(raw_input("Please enter the word you would to have checked:"))
+		givenword = str(raw_input("Please enter the word you would like to have checked:"))
 	else:
-		if givenword == givenword[::-1]: # Using an extended index to determine if the word given matches its reverse
+		if givenword == givenword[::-1]: # An extended index slice determines if the word given matches its reverse.
 			print "Your word is a palindrome."
 		else:
 			print "Your word is not a palindrome."
