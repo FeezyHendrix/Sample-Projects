f9b15c1bf7e6c18f9e54eb752abd14f4dad40232
mandeepbhutani <bhutanimandeep@gmail.com>
2015-01-31 02:09:04 -0600

    Count Vowels

---
+++ CountVowel.py
@@ -0,0 +1,21 @@
+"""
+Project: Projects Repo - Count Vowels
+
+Author: Mandeep Bhutani
+
+Date: 1/31/2015
+
+Problem: Enter a string and the program counts the number of vowels in the text. 
+For added complexity have it report a sum of each vowel found.
+
+References: 
+"""
+result = str(raw_input("Please enter text in order to count the number of vowels:"))
+total = 0 
+for x in result:
+	if x in "AEIOUaeiou": # Python allows for the iteration of strings using the word 'in'.
+		total += 1 # For each vowel found, add 1 to the total.
+if total > 1: # In order to print correct grammer, prints a sentence based on the number of vowels.
+	print "There were %s vowels found." % total
+else:
+	print "There was %s vowel found." % total
