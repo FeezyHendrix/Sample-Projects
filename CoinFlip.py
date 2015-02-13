bf08a3af8ebd0b292e6cba9a9e7cc161a2792de8
mandeepbhutani <bhutanimandeep@gmail.com>
2015-02-13 17:55:33 -0600

    Coin Flip

--- CoinFlip.py
+++ CoinFlip.py
@@ -1,10 +1,17 @@
-import random
+"""
+Title: Projects Repo - Coin Flip
+Author: Mandeep Bhutani
+Date: 2/13/2015
+Problem: Simulate a coin flip. 
+"""
+import random # Module that will choose between heads or tails
 
 count_heads = 0
 count_tails = 0
 
-while True:
-    guess = random.choice([0,1])
+while True: # While command loops until user no longer wishes to continue
+
+    guess = random.choice([0,1]) # Chooses between 0 and 1
     play = str(raw_input("Would you like to flip a coin? Yes or No?"))
 
     if play.lower() != "yes":
