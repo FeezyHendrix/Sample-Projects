38e2b9d8049a0bb9211297b12bda694570649fd6
mandeepbhutani <bhutanimandeep@gmail.com>
2015-02-15 00:01:17 -0600

    Coin Flip Simulation

--- CoinFlip.py
+++ CoinFlip.py
@@ -2,16 +2,16 @@
 Title: Projects Repo - Coin Flip
 Author: Mandeep Bhutani
 Date: 2/13/2015
-Problem: Simulate a coin flip. 
+Problem: Simulate a coin flip.
 """
-import random # Module that will choose between heads or tails
+import random  # Module that will choose between heads or tails
 
 count_heads = 0
 count_tails = 0
 
-while True: # While command loops until user no longer wishes to continue
+while True:  # While command loops until user no longer wishes to continue
 
-    guess = random.choice([0,1]) # Chooses between 0 and 1
+    guess = random.choice([0, 1])  # Chooses between 0 and 1
     play = str(raw_input("Would you like to flip a coin? Yes or No?"))
 
     if play.lower() != "yes":
@@ -23,4 +23,5 @@ while True: # While command loops until user no longer wishes to continue
     else:
         count_tails += 1
         print "Tails"
-    print "The coin has flipped %s Heads and %s Tails." % (count_heads, count_tails)
+    print "The coin has flipped %s Heads and %s Tails." \
+        % (count_heads, count_tails)
