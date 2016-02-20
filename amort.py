290af49fc63055484efaaf33c54c508246012483
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-19 23:16:09 -0600

    Added docstring

--- amort.py
+++ amort.py
@@ -1,3 +1,19 @@
+"""
+Title: Sample Projects - Monthly Payment Calculator
+Author: Mandeep Bhutani
+Date: 02/18/2018
+Problem: Calculate the monthly payments of a fixed term mortgage over given
+Nth terms at a given interest rate. Also figure out how long it will take the
+user to pay back the loan. For added complexity, add an option for users to
+select the compounding interval (Monthly, Weekly, Daily, Continually).
+
+Analysis: The  present value of annuity formula is used to solve this
+problem and takes the form of: A = i * p * (1 + i)^n / (1 + i)^n - 1
+Where A is the monthly payment, p is the present value of the borrowed
+amount, i is the period's interest rate, and n is the total number of
+payments. While the variables chosen are not pythonic, they do correspond
+to the annuity formula variables.
+"""
 p = float(input('Enter the amount borrowed:'))
 t = float(input('Enter the length of the loan (in years):'))
 q = float(input('Enter the number of pay periods per year:'))
