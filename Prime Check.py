1843c5861ec67b4f9983e508ced06cdd69795578
mandeepbhutani <bhutanimandeep@gmail.com>
2016-06-23 21:48:21 -0500

    Updated to account for numbers less than 2

--- Prime Check.py
+++ Prime Check.py
@@ -9,8 +9,9 @@ from math import sqrt
 
 
 def is_prime(n):
-    for x in range(2, int(sqrt(n)+1)):
-        if n % x == 0:
+    if n < 2:
+        return False
+    for i in range(2, int(sqrt(n)+1)):
+        if n % i == 0:
             return False
-    else:
-        return True
+    return True
