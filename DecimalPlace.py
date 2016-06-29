e7accd2fbc79106a8a28c8f4a2c3f18666a75cc9
mandeepbhutani <bhutanimandeep@gmail.com>
2016-06-29 12:53:49 -0500

    Fixed docstring comment

--- DecimalPlace.py
+++ DecimalPlace.py
@@ -16,7 +16,7 @@ from decimal import Decimal, getcontext
 
 
 def bailey_borwein_plouffe(precision):
-    """The precision argument defines the length of Pi."""
+    """The precision argument is an integer that defines the length of Pi."""
     getcontext().prec = precision
     formula = sum(1 / Decimal(16) ** k * (Decimal(4) / (8 * k + 1) -
                                           Decimal(2) / (8 * k + 4) -
