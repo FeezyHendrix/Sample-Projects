73ada66fe08f01078233c2f2220cbe34285aba21
mandeepbhutani <bhutanimandeep@gmail.com>
2016-06-29 11:31:45 -0500

    Moved comment to same line as variable assignment

--- DecimalPlace.py
+++ DecimalPlace.py
@@ -16,8 +16,7 @@ from decimal import Decimal, getcontext
 
 
 def bailey_borwein_plouffe(precision):
-    # getcontext.prec() defines the length of Pi
-    getcontext().prec = precision
+    getcontext().prec = precision  # getcontext.prec() defines the length of Pi
     formula = sum(1 / Decimal(16) ** k * (Decimal(4) / (8 * k + 1) -
                                           Decimal(2) / (8 * k + 4) -
                                           Decimal(1) / (8 * k + 5) -
