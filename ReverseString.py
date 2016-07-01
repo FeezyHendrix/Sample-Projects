e88f1e20e4a26627b722f52a46cf943ec67d26e0
mandeepbhutani <bhutanimandeep@gmail.com>
2016-06-30 19:17:42 -0500

    Refactored code

--- ReverseString.py
+++ ReverseString.py
@@ -1,14 +1,12 @@
 """
-Project: Sample Project from Projects Repo - Reverse a String
+Project: Mega Projects List - Reverse a String
 Author: Mandeep Bhutani
-Date: 1/29/2015
+Date: 01/29/2015
 
 Problem: Enter a string and the program will reverse it and print it out.
-
-References: https://docs.python.org/2.3/whatsnew/section-slices.html
 """
 
-oldstring = str(raw_input("Please enter the string you would like to reverse:"))
-newstring = oldstring[::-1] # This is an extended slice in the format of [start:stop:step]. The -1 step 
+word = str(input("Please enter the string you would like to reverse:"))
+reversed_word = word[::-1] # This is an extended slice in the format of [start:stop:step]. The -1 step 
 # reverses the order of its assignment.
-print newstring
+print(reversed_word)
