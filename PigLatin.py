aceddbb5f0d20894f3023dbde3d53365d7977484
mandeepbhutani <bhutanimandeep@gmail.com>
2016-06-30 19:15:23 -0500

    Refactored code and added py3 compatibility

--- PigLatin.py
+++ PigLatin.py
@@ -6,17 +6,15 @@ Date: 1/29/2015
 Problem:  Pig Latin is a game of alterations played on the English language game. 
 To create the Pig Latin form of an English word the initial consonant sound is transposed
 to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay).
-
-References: 
 """
-oldword = str(raw_input("Please enter a word to be translated:"))
-while not oldword.isalpha(): # Checks to make sure that the input word is a real word.
-	print "Please enter a new word with alpha characters only."
-	oldword = str(raw_input("Please enter a word to be translated:"))
+word = str(input("Please enter a word to be translated: "))
 
+while not word.isalpha():
+    word = str(input("Please enter a new word with alpha characters only: "))
+
+if word[0] not in 'aeiouAEIOU':  # Checks input word to see if first letter is a consonant
+    pig_latin_word = word[1:len(word)] + word[0] + 'ay'
 else:
-	if oldword[0] not in 'aeiouAEIOU': # Checks input word to see if first letter is a consonant
-		newword = oldword[1:len(oldword)] + oldword[0] + 'ay' # Using index slices to return the correct letters
-	else:
-		newword = oldword + 'ay' # If the first letter is a vowel, then only the 'ay' ending is adding.
-	print newword.capitalize() # Captalizes the first letter of the word
+    pig_latin_word = word + 'ay'  # If the first letter is a vowel, then only the 'ay' ending is adding.
+
+print(pig_latin_word.capitalize())
