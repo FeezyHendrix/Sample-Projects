caf342c8814f5f1978ed2df560b2e48a068dc86b
mandeepbhutani <bhutanimandeep@gmail.com>
2016-06-30 19:06:17 -0500

    Refactored code and added py3 compatibility

--- Palindrome.py
+++ Palindrome.py
@@ -6,14 +6,16 @@ Date: 1/30/2015
 Problem: Checks if the string entered by the user is a palindrome.
 References: https://docs.python.org/2.3/whatsnew/section-slices.html
 """
+
+
 def palindrome():
-	givenword = str(raw_input("Please enter the word you would like to have checked:"))
-	while not givenword.isalpha(): # Checks to make sure all characters are alpha characters.
-		print "Please enter a real word."
-		givenword = str(raw_input("Please enter the word you would like to have checked:"))
-	else:
-		if givenword == givenword[::-1]: # An extended index slice determines if the word given matches its reverse.
-			print "Your word is a palindrome."
-		else:
-			print "Your word is not a palindrome."
-palindrome()
+    word = str(input("Please enter the word you would like to have checked: "))
+    while not word.isalpha():
+        print("Please enter a real word.")
+        word = str(input("Please enter the word you would like to have checked: "))
+    if word == word[::-1]: # An extended index slice determines if the word given matches its reverse.
+        return "Your word is a palindrome."
+    else:
+        return "Your word is not a palindrome."
+
+print(palindrome())
