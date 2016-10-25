4ef4559969a49b1e01c631642ad316eda10e9927
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-24 21:13:02 -0500

    Switched from i64 to u64 integers

--- Sieve_of_Eratosthenes.rs
+++ Sieve_of_Eratosthenes.rs
@@ -1,9 +1,9 @@
 use::std::collections::HashSet;
 
 
-fn sieve_of_erastosthenes(n: i64) -> HashSet<i64> {
-    let mut primes: HashSet<i64> = HashSet::new();
-    let mut multiples: HashSet<i64> = HashSet::new();
+fn sieve_of_erastosthenes(n: u64) -> HashSet<u64> {
+    let mut primes: HashSet<u64> = HashSet::new();
+    let mut multiples: HashSet<u64> = HashSet::new();
     for i in 2..n+1 {
         if !multiples.contains(&i) {
             primes.insert(i);
